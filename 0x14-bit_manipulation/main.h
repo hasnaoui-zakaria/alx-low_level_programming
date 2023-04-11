999eb2851e708ec4ea951fa5d3c613c0fd73fece
Habou Zakaria <habouzakaria1907@gmail.com>
2023-04-10 19:32:40 -0700

    fix task 1

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,3 +1,5 @@
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
@@ -7,3 +9,5 @@ int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
+
+#endif
